a3617b92f18123c840a8a4fcbc798493ce2c8216
b10t <b10t.rus@gmail.com>
2022-02-11 16:31:33 +0800

    Добавлена функция по созданию благодарности.

--- datacenter/management/commands/scripts.py
+++ datacenter/management/commands/scripts.py
@@ -1,6 +1,9 @@
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from random import choice
+
 from datacenter.models import *
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.core.management.base import BaseCommand
+
 
 def find_schoolkid(full_name: str):
     """Поиск ученика в базе данных по ФИО.
@@ -20,22 +23,47 @@ def find_schoolkid(full_name: str):
 
 
 def fix_marks(schoolkid):
+    """Исправляет плохие оценки ученика на 5.
+
+    Args:
+        schoolkid (Schoolkid): Ученик
+    """
     for mark in Mark.objects.filter(schoolkid=schoolkid, points__lte=3):
         mark.points = 5
         mark.save()
 
 
-def del_chastisements(schoolkid):
+def remove_chastisements(schoolkid):
+    """Удаляет замечание по ученику.
+
+    Args:
+        schoolkid (Schoolkid): Ученик
+    """
     for chastisement in Chastisement.objects.filter(schoolkid=schoolkid):
         chastisement.delete()
 
 
-def create_commendation(full_name, subject_title):
-    schoolkid = Schoolkid.objects.get(full_name__contains=full_name)
-    # schoolkid = Schoolkid.objects.filter(full_name__contains='Фролов Иван')
-    # lesson = Lesson.objects.filter(subject__title=subject_title, year_of_study=6, group_letter='А').order_by('-date').first()
+def create_commendation(schoolkid, subject_title):
+    """Создание благодарности по ученику.
 
-    print('schoolkid123')
+    Args:
+        schoolkid (Schoolkid): Ученик
+        subject_title (str): Название предмета
+    """
+    with open('./commendations.txt', 'r') as commendations_file:
+        commendation_phrase = choice(commendations_file.readlines()).strip()
+
+        lesson = Lesson.objects.filter(
+            subject__title=subject_title,
+            year_of_study=schoolkid.year_of_study,
+            group_letter=schoolkid.group_letter
+        ).order_by('-date').first()
+
+        Commendation.objects.create(text=commendation_phrase,
+                                    created=lesson.date,
+                                    schoolkid=schoolkid,
+                                    subject=lesson.subject,
+                                    teacher=lesson.teacher)
 
 
 class Command(BaseCommand):
@@ -44,5 +72,8 @@ class Command(BaseCommand):
     help = 'Hack DB'
 
     def handle(self, *args, **options):
-        # create_commendation('Фролов Иван', 'Музыка')
-        print(find_schoolkid('Фролов Иван'))
+        schoolkid = find_schoolkid('Фролов Иван')
+        if schoolkid:
+            create_commendation(schoolkid, 'Математика')
+        # print(find_schoolkid('Фролов Иван'))
+        #
